013270fd42f5ed766b6ca5f223c589c6389cfdea
free-sopi <mhsop@naver.com>
2019-09-14 03:42:22 +0900

    first androit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onclickButton(View v){
+        Toast.makeText(getApplicationContext(),"버튼이 눌렸음",Toast.LENGTH_LONG).show();
+
+    }
+
+    public void onclickButton2(View v){
+        Intent intent= new Intent(Intent.ACTION_VIEW, Uri.parse("http://m.naver.com");
+        startActivity(intent);
+    }
+}
